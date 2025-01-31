09331614d310d754079240486a70153124a7e811
SChandrakanth15 <schandrakanth15@gmail.com>
2025-01-31 18:08:31 +0530

    first commit

---
+++ src/test/java/com/elixrlabs/User_Authentication_Service/UserAuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elixrlabs.User_Authentication_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
